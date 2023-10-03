643a4428cc42f4f2e1c1858ee46400d5732b1000
Tiago Raimundi <raimunditiagoaugusto@gmail.com>
2023-10-03 18:44:59 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function randomID(len) {
+  let result = '';
+  if (result) return result;
+  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+    maxPos = chars.length,
+    i;
+  len = len || 5;
+  for (i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
 }
 
-export default App;
+export function getUrlParams(
+  url = window.location.href
+) {
+  let urlStr = url.split('?')[1];
+  return new URLSearchParams(urlStr);
+}
+
+export default function App() {
+  const roomID = getUrlParams().get('roomID') || randomID(5);
+  let role_str = getUrlParams(window.location.href).get('role') || 'Host';
+  const role =
+    role_str === 'Host'
+      ? ZegoUIKitPrebuilt.Host
+      : role_str === 'Cohost'
+        ? ZegoUIKitPrebuilt.Cohost
+        : ZegoUIKitPrebuilt.Audience;
+
+  let sharedLinks = [];
+  if (role === ZegoUIKitPrebuilt.Host || role === ZegoUIKitPrebuilt.Cohost) {
+    sharedLinks.push({
+      name: 'Join as co-host',
+      url:
+        window.location.protocol + '//' +
+        window.location.host + window.location.pathname +
+        '?roomID=' +
+        roomID +
+        '&role=Cohost',
+    });
+  }
+  sharedLinks.push({
+    name: 'Join as audience',
+    url:
+      window.location.protocol + '//' +
+      window.location.host + window.location.pathname +
+      '?roomID=' +
+      roomID +
+      '&role=Audience',
+  });
+  // generate Kit Token
+  const appID = 1698910691;
+  const serverSecret = "19ce6d3e8c8f352a9e310c04d58ebada";
+  const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, randomID(5), randomID(5));
+
+
+  // start the call
+  let myMeeting = async (element) => {
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.LiveStreaming,
+        config: {
+          role,
+        },
+      },
+      sharedLinks,
+    });
+  };
+
+  return (
+    <div
+      className="myCallContainer"
+      ref={myMeeting}
+      style={{ width: '100vw', height: '100vh' }}
+    ></div>
+  );
+}
